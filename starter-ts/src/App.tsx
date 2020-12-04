dd69f849eb3412ef2d87a309c056145369383797
kyletsang <6854874+kyletsang@users.noreply.github.com>
2020-12-04 11:26:18 -0800

    Add starter-ts

---
+++ starter-ts/src/App.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Button, Container, Jumbotron } from 'reactstrap';
+
+function App() {
+  return (
+    <Container>
+      <Jumbotron>
+        <h1 className="display-4">Hello, Reactstrap!</h1>
+        <p className="lead">
+          This is a simple hero unit, a simple jumbotron-style component for
+          calling extra attention to featured content or information.
+        </p>
+        <hr className="my-4" />
+        <p>
+          It uses utility classes for typography and spacing to space content
+          out within the larger container.
+        </p>
+        <Button color="primary">Learn more</Button>
+      </Jumbotron>
+    </Container>
+  );
+}
+
+export default App;
